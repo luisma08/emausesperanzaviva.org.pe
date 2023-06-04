b4a2f9d7a76dbd55f7f5a470e3dac2582b94c80d
Luis <luismayta08@gmail.com>
2023-06-03 20:25:27 -0500

    creando landing page

---
+++ components/header-landing.php
@@ -0,0 +1,43 @@
+<header class="" id="">
+    <section class="social" id="social">
+        <div class="container">
+            <ul class="social__nav-list g-elements g-template-columns-3fr gap-sm">
+                <li class="d-responsive--reverse f-elements gap-sm">
+                    <a href="" class="social__nav-link my-auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill social__nav-img" color="#1F8949" viewBox="0 0 16 16">
+                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                        </svg>
+                    </a>
+                    <a href="" class="list__link my-auto" id="social-text-1">Asentamiento Humano Villa Solidaridad 3ra etapa Mz. k4 Lte. 5 - San Juan de Miraflores</a>
+                </li>
+                <li class="social__nav-item f-elements f-elements--center gap-sm">
+                    <a href="tel:963344569" class="social__nav-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-headset social__nav-img" color="#1F8949" viewBox="0 0 16 16">
+                            <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z"/>
+                        </svg>
+                    </a>
+                    <a href="tel:017577908" class="list__link" id="social-text-1">757-7908</a>
+                    <a href="" class="list__link">|</a>
+                    <a href="tel:963344569" class="list__link" id="social-text-1">963 344 569</a>
+                </li>
+                <li class="social__nav-item f-elements f-elements--center gap-sm">
+                    <a href="mailto:donaciones@emausesperanzavivaperu.org" class="social__nav-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope social__nav-img" color="#1F8949" viewBox="0 0 16 16">
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                    </a>
+                    <a href="mailto:donaciones@emausesperanzavivaperu.org" class="list__link" id="social-text-2">donaciones@emausesperanzavivaperu.org</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <div class="container bg-white">
+        <nav class="nav f-elements f-elements--responsive-2 f-col md:f-row" id="header-nav">
+            <div class="header__container-logo">
+                <a href="./">
+                    <img src="./assets/img/logo.webp" alt="logo" class="nav__logo">
+                </a>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ landing.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/css/style.css">
+    <link rel="stylesheet" href="./styles/css/fe.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
+    />
+
+    <!-- FONT FAMILY -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- ICON -->
+    <link rel="shortcut icon" href="assets/icon/favicon.webp" type="image/svg+xml">
+
+    <!-- HTML Meta Tags -->
+    <title>▷ Emaús - Esperanza Viva [ Donaciones ]</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+</head>
+<body class="body">
+    <!-- Agregando componente header -->
+    <?php include('components/header-landing.php') ?>
+    <!-- FIN -->
+
+    <main class="main">
+        <section class="section">
+            <div class="container">
+                <div class="f-elements f-col md:f-row md:f-row--reverse gap-md">
+                    <div class="f-elements f-col md:w-50">
+                        <div class="f-elements f-col">
+                            <img src="./assets/img/wsp-hero.webp" alt="logo" class="section__img" />
+                        </div>
+                        <div class="">
+                            <a class="button button--primary" href="#contact"><i class="bi bi-balloon-heart"></i> deseo donar</a>
+                        </div>
+                    </div>
+                    <div class="f-elements f-col f-elements--center md:w-50">
+                        <div class="f-elements f-col gap-md">
+                            <h1 class="section__title section__title--secondary">Dona con esperanza y transforma el futuro.</h1>
+                            <p class="section__description">
+                                Impacta vidas. Sé parte del cambio donando hoy
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section bg-dark-green">
+            <div class="container">
+                <div class="f-elements f-elements--responsive gap-sm border p-responsive--cta bg-parallax">
+                    <div class="f-elements f-col">
+                        <p class="section__title section__title--fourt">!Actúa ahora y sé parte del cambio. Dona y comparte esperanza!</p>
+                        <p class="section__text">Tu aporte marca la diferencia para más familias</p>
+                    </div>
+                    <div class="f-elements f-col f-elements--center">
+                        <a class="button button--outline-primary" href="#contact"><i class="bi bi-balloon-heart"></i> deseo donar</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+</body>
+</html>
