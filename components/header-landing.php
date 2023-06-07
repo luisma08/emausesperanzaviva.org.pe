b27b4abdebab8e74978810c4f512d86f0fdf05f0
Luis <luismayta08@gmail.com>
2023-06-06 19:12:02 -0500

    corrigiendo header y banner

--- components/header-landing.php
+++ components/header-landing.php
@@ -2,7 +2,7 @@
     <section class="social" id="social">
         <div class="container">
             <ul class="social__nav-list g-elements g-template-columns-3fr gap-sm">
-                <li class="d-responsive--reverse f-elements gap-sm">
+                <li class="d-responsive--reverse gap-sm">
                     <a href="" class="social__nav-link my-auto">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill social__nav-img" color="#1F8949" viewBox="0 0 16 16">
                             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
